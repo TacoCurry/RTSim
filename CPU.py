9811b52592a1b01903e0428edb376ecad3d41808
QueenCurry <yeawonjo@gmail.com>
2019-10-03 22:05:03 +0900

    Create subclasses of CPU
    CPU를 추상클래스로 만들고, 서브클래스인 None-DVFS-CPU와 DVFS-CPU를 추가했어요.

--- CPU.py
+++ CPU.py
@@ -1,3 +1,5 @@
+from abc import *
+
 
 class CpuFrequency:
     def __init__(self, wcet_scale, power_active, power_idle):
@@ -6,26 +8,47 @@ class CpuFrequency:
         self.power_idle = power_idle
 
 
-class CPU:
-    cpufreqs = []
-    n_cpufreqs = 0
+class CPU(metaclass=ABCMeta):
     MAX_CPU_FREQS = 15
 
-    @staticmethod
-    def insert_cpufreq(wcet_scale, power_active, power_idle) -> bool:
-        if CPU.n_cpufreqs >= CPU.MAX_CPU_FREQS:
-            return False
-        if CPU.n_cpufreqs > 0 & CPU.cpufreqs[CPU.n_cpufreqs-1].wcet_scale < wcet_scale:
+    def __init__(self):
+        self.cpufreqs = []
+        self.n_cpufreqs = 0
+
+    def insert_cpufreq(self, wcet_scale, power_active, power_idle) -> bool:
+        if self.n_cpufreqs >= CPU.MAX_CPU_FREQS:
             return False
 
-        CPU.n_cpufreqs += 1
-        CPU.cpufreqs.append(CpuFrequency(wcet_scale, power_active, power_idle))
+        self.n_cpufreqs += 1
+        self.cpufreqs.append(CpuFrequency(wcet_scale, power_active, power_idle))
+        self.sort_cpufreq()
         return True
 
-    @staticmethod
-    def sort_cpufreq():
+    def sort_cpufreq(self):
+        self.cpufreqs.sort(key=lambda object: object.wcet_scale, reverse=True)
+
+    def assign_cpufreq(self, task):
+        task.cpu_frequency = self.cpufreqs[0]
+
+    @abstractmethod
+    def reassign_cpufreq(self, task) -> bool:
         pass
 
+
+class NoneDVFSCPU(CPU):
+    def reassign_cpufreq(self, task) -> bool:
+        return True
+
+
+class DVFSCPU(CPU):
+    def reassign_cpufreq(self, task) -> bool:
+        for cpufreq in self.cpufreqs:
+            task.cpu_frequency = cpufreq
+            task.calc_task_det()
+            if task.is_schedulable():
+                return True
+            task.revert_task_det()
+        return False
 
 
 
