14ab9327d701f190d4f472924ec6293e360b4889
QueenCurry <yeawonjo@gmail.com>
2019-10-03 22:05:46 +0900

    Fix: Main
    Simulator.py -> Main.py
    Main에서는 Policy 선택만 받고, 나머지는 Policy(System)안에서 수행하도록 변경했어요.

---
+++ Main.py
@@ -0,0 +1,17 @@
+from System import Dram, Hm, DvfsDram, DvfsHm, System
+
+
+def get_system(policy_num: int) -> System:
+    if policy_num == 1:
+        return Dram()
+    elif policy_num == 2:
+        return Hm()
+    elif policy_num == 3:
+        return DvfsDram()
+    elif policy_num == 4:
+        return DvfsHm()
+
+
+get_system(int(input("Policy를 선택하세요.\n[1]DRAM, [2]HM. [3]DVFS_DRAM, [4]DVFS_HM : "))).run()
+
+
